5fddf422f546b6eafd7d37df85dbf98d1a4b9c1c
leeteukki <tklohj@hanyang.ac.kr>
2020-07-05 15:28:54 +0900

    Initial Commit

---
+++ login_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  login_swiftui
+//
+//  Created by lee teukki on 2020/07/05.
+//  Copyright © 2020 lee teukki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
